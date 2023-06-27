f20032eccf681f4a8838d8bc262741ba007de20c
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-06-27 12:43:23 +0300

    email for sending $ change DB name according to postgresDB ;I created

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -71,10 +71,18 @@ WSGI_APPLICATION = 'auth_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # Name posttgres DB
+        'NAME': 'auth_system',
     }
 }
 
+# EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 597
+EMAIL_HOST_USER = "docs.hezzy@gmail.com"
+EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_TLS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
